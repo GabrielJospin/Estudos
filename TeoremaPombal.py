663b2b468fd5ece04cff02b2b7425f6c8df51e06
Gabriel Jospin <gabi.jospin@gmail.com>
2020-03-12 18:26:36 -0300

    YourName

--- TeoremaPombal.py
+++ TeoremaPombal.py
@@ -4,10 +4,13 @@
 def FindNumber(Array):
 	comp = len(Array)
 	i = 0
+	NewArray = []
 	while i < comp:
 		j = 1
 		while j < comp :
-			if Array [i] == Array [j] and i != j:
-				print (Array [i])
+			if Array [i] == Array [j] and i < j:
+				print(Array [i])
 			j +=1
+
 		i+=1
+		
