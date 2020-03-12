b07945769d55d44232a3750fc9206f61603d7b92
Gabriel Jospin <gabi.jospin@gmail.com>
2020-03-11 21:29:00 -0300

    adicionando Teorema Pombal com bugs

---
+++ TeoremaPombal.py
@@ -0,0 +1,13 @@
+#Encontre a duplicato
+
+
+def FindNumber(Array):
+	comp = len(Array)
+	i = 0
+	while i < comp:
+		j = 1
+		while j < comp :
+			if Array [i] == Array [j] and i != j:
+				print (Array [i])
+			j +=1
+		i+=1
